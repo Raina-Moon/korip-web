97f5c0172424704d3d650523e6885107d8dbb431
Raina Moon <mds6425@gmail.com>
2025-05-24 05:40:36 +0900

    ✨ feat: implement HeaderBar component with language selection and logo

--- src/app/components/HeaderBar.tsx
+++ src/app/components/HeaderBar.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 const HeaderBar = () => {
-  return (
-    <div>HeaderBar</div>
-  )
-}
+  const [select, setSelect] = useState("");
+  const router = useRouter();
 
-export default HeaderBar
+  const languages = [
+    { name: "English", code: "en" },
+    { name: "한국어", code: "kr" },
+  ];
+
+  return (
+    <div className="flex justify-between items-center px-5 border-b border-primary-800">
+      <div>
+        <Image
+          src="/images/koriplogo.png"
+          alt="korip logo"
+          onClick={() => router.push("/")}
+          width={100}
+          height={100}
+        />
+      </div>
+      <div className="flex items-center gap-4 pr-3">
+        <button value={select}>Language</button>
+        <button onClick={() => router.push("/login")}>
+          <i className="bi bi-person-circle text-primary-800 text-3xl"></i>
+        </button>
+      </div>
+
+      {select && (
+        <div>
+          <select
+            value={select}
+            onChange={(e) => {
+              setSelect(e.target.value);
+            }}
+          >
+            {languages.map((lang) => (
+              <option key={lang.code} value={lang.code}>
+                {lang.name}
+              </option>
+            ))}
+          </select>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HeaderBar;
