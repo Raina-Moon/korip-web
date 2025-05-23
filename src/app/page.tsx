0c12d17f6bf73a60d657c9fb23aeaabafaf409fd
Raina Moon <mds6425@gmail.com>
2025-05-24 04:56:11 +0900

    ✨ feat: update search component structure and remove unused filtered data logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,33 @@
+"use client";
+
 import React, { useState } from "react";
 
 const page = () => {
   const [search, setSearch] = useState("");
-  const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
-
-  const filteredData = data.filter((item) => item.name.toLowerCase().includes(search.toLowerCase()));
 
   const handleSearch = () => {};
 
   return (
+    <>
     <div>
       <input
         type="text"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
-     {filteredData.map((i) => (
-      <li>
-        <p>{i}</p>
-      </li>
-     ))}
-     [filteredData.length === 0 && <p>No results found</p>]
+      <button>Search</button>
     </div>
+
+    <div>
+      <p>News</p>
+    </div>
+
+    <div>
+      <p>Events</p>
+    </div>
+    </>
   );
 };
 
