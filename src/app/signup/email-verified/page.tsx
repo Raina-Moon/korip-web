55a391c553c5683fb0849c861a8d64541dd533c4
Raina Moon <mds6425@gmail.com>
2025-05-26 17:49:15 +0900

    💄ui: enhance EmailVerifPage layout with improved styling and structure

--- src/app/signup/email-verified/page.tsx
+++ src/app/signup/email-verified/page.tsx
@@ -27,21 +27,51 @@ const EmailVerifPage = () => {
       alert("Sign up failed. Please try again.");
     }
   };
-  return (
-    <div>
-      <p>Email</p>
-      <input value={email} onChange={(e) => setEmail(e.target.value)} />
-      <p>Nickname</p>
-      <input value={nickname} onChange={(e) => setNickname(e.target.value)} />
-      <p>Password</p>
-      <input value={password} onChange={(e) => setPassword(e.target.value)} />
-      <p>Confirm Password</p>
-      <input
-        value={confirmPassword}
-        onChange={(e) => setConfirmPassword(e.target.value)}
-      />
 
-      <button>Sign Up</button>
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
+      <div className="flex flex-col gap-4">
+        <div>
+          <p className="text-primary-800 font-medium text-sm">Email</p>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="border border-primary-700 rounded-sm px-2 py-1 outline-none"
+          />
+        </div>
+        <div>
+          <p className="text-primary-800 font-medium text-sm">Nickname</p>
+          <input
+            value={nickname}
+            onChange={(e) => setNickname(e.target.value)}
+            className="border border-primary-700 rounded-sm px-2 py-1 outline-none"
+          />
+        </div>
+        <div>
+          <p className="text-primary-800 font-medium text-sm">Password</p>
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="border border-primary-700 rounded-sm px-2 py-1 outline-none"
+            type="password"
+          />
+        </div>
+        <div>
+          <p className="text-primary-800 font-medium text-sm">
+            Confirm Password
+          </p>
+          <input
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            className="border border-primary-700 rounded-sm px-2 py-1 outline-none"
+            type="password"
+          />
+        </div>
+      </div>
+
+      <button className="bg-primary-700 text-white px-2 py-1 rounded-md hover:bg-primary-500">
+        Sign Up
+      </button>
     </div>
   );
 };
