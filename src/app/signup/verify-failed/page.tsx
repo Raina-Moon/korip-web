826959a4dd31a631ec07952d813b564d6edc3cad
Raina Moon <mds6425@gmail.com>
2025-05-26 17:53:40 +0900

    ✨feat: implement VerifFailedPage component for handling email verification failures

--- src/app/signup/verify-failed/page.tsx
+++ src/app/signup/verify-failed/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+"use client";
+
+import { useRouter } from "next/navigation";
+import React from "react";
 
 const VerifFailedPage = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      <h1 className="text-3xl font-semibold text-primary-800">
+        Failed Email Verification
+      </h1>
+      <p className="text-primary-800 mt-4">
+        It seems that your email verification has failed. Please try signing up
+        again or contact support if you need assistance.
+      </p>
+      <button
+        onClick={() => useRouter().push("/signup/email")}
+        className="bg-primary-700 text-white px-4 py-2 rounded-md mt-4 hover:bg-primary-500"
+      >
+        Go Back to Email Verification
+      </button>
+    </div>
+  );
+};
 
-export default VerifFailedPage
+export default VerifFailedPage;
