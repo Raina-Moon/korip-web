b57f066ce744b1c749cafc1d30877f131c765bb4
Raina Moon <mds6425@gmail.com>
2025-05-27 01:47:59 +0900

    ✨feat: add show/hide password functionality in LoginPage

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -20,14 +20,28 @@ const LoginPage = () => {
         <input
           type="email"
           className="border border-primary-800 rounded-md outline-none px-3 py-1"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
       </div>
       <div className="flex flex-col gap-1">
         <p className="text-primary-800 text-sm font-medium">Password</p>
+        <div className="relative">
+
         <input
-          type="password"
+          type={showPassword ? "text" : "password"}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           className="border border-primary-800 rounded-md outline-none px-3 py-1"
         />
+         <button
+            type="button"
+            onClick={() => setShowPassword((prev) => !prev)}
+            className="absolute right-2 top-1/2 -translate-y-1/2 text-sm text-primary-700 hover:underline"
+            >
+            {showPassword ? <i className="bi bi-eye-slash"></i> : <i className="bi bi-eye"></i>}
+          </button>
+              </div>
       </div>
       <button
         onClick={handleLogin}
