b63758fc1e3a5ee107f573ccc8f9b7afc66b6c83
Raina Moon <mds6425@gmail.com>
2025-05-27 14:36:55 +0900

    ♻️refactor : wrap application in GoogleOAuthProvider for OAuth integration

--- src/app/lib/providers.tsx
+++ src/app/lib/providers.tsx
@@ -2,9 +2,13 @@
 
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export function Providers({children}: {children: React.ReactNode}) {
   return (
+    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || ""}>
+
     <Provider store={store}>{children}</Provider>
+    </GoogleOAuthProvider>
   );
 }
