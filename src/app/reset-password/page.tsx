73eb5ff69429bcd0fe37fd7e0056e2b68f3acad9
Raina Moon <mds6425@gmail.com>
2025-05-27 17:33:00 +0900

    ✨feat : create Reset Password page component with email input and submission logic

---
+++ src/app/reset-password/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import React, { useState } from "react";
+
+const ResetPwdPage = () => {
+  const [email, setEmail] = useState("");
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      router.push(`/reset-password/${email}?token=${params.get("token")}`);
+    } catch (err) {
+      console.error("Error during password reset:", err);
+    }
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
+      <h2 className="text-3xl">We'll send you the code</h2>
+      <input
+        type="email"
+        id="email"
+        placeholder="email@example.com"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className="border border-primary-700 rounded-md px-2 py-1 outline-none w-full max-w-md"
+      />
+      <button onSubmit={handleSubmit} className="bg-primary-700 text-white px-2 py-1 rounded-md hover:bg-primary-500">Send</button>
+      <p className="text-primary-800 text-sm">
+          Don't have an account?{" "}
+          <a href="/signup/email" className="text-primary-700 hover:underline">
+            Sign Up
+          </a>
+        </p>
+    </div>
+  );
+};
+
+export default ResetPwdPage;
