546e866f046376d199f80987880a2c4d2308b6e7
Raina Moon <mds6425@gmail.com>
2025-05-28 03:22:21 +0900

    🚚 move : move hooks and store into store folder

--- src/app/lib/store/store.ts
+++ src/app/lib/store/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { authApi } from "./auth/authApi";
-import authReducer from "./auth/authSlice";
-import resetPasswordReducer from "./reset-password/resetPasswordSlice";
+import { authApi } from "../auth/authApi";
+import authReducer from "../auth/authSlice";
+import resetPasswordReducer from "../reset-password/resetPasswordSlice";
 
 export const store = configureStore({
     reducer: {
