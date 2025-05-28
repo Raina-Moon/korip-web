909b3aed3a4e7bb24a3499b72797fe76c02e45a2
Raina Moon <mds6425@gmail.com>
2025-05-28 18:28:27 +0900

    ✨ feat: implement logoutUser thunk for user session termination

---
+++ src/app/lib/auth/logoutThunk.ts
@@ -0,0 +1,14 @@
+import { AppDispatch } from "../store/store";
+import { authApi } from "./authApi";
+import { logout } from "./authSlice";
+
+export const logoutUser = () => async (dispatch: AppDispatch) => {
+    try {
+        await dispatch(authApi.endpoints.logout.initiate({})).unwrap()
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        dispatch(logout())
+    } catch (err) {
+        console.error("Logout failed:", err);
+    }
+} 
