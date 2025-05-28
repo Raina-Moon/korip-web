93d7a119c1fc2f8206c83c5ea3036817d1eb110e
Raina Moon <mds6425@gmail.com>
2025-05-28 18:27:52 +0900

    ✨ feat: enhance Axios interceptor to handle 401 errors with token refresh logic

--- src/app/lib/axios/interceptor.ts
+++ src/app/lib/axios/interceptor.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { store } from "../store/store";
 import { refreshTokenThunk } from "../auth/refrestTokenThunk";
+import { logout } from "../auth/authSlice";
 
 export const setupAxiosInterceptor = () => {
   axios.interceptors.response.use(
@@ -8,18 +9,28 @@ export const setupAxiosInterceptor = () => {
     async (err) => {
       const originalRequest = err.config;
 
-      if (err.response.status === 401 && !originalRequest._retry) {
+      if (
+        err.response?.status === 401 &&
+        !originalRequest._retry &&
+        !originalRequest.url.includes("/v1/auth/refresh") &&
+        !originalRequest.url.includes("/v1/auth/login")
+      ) {
         originalRequest._retry = true;
-        await store.dispatch<any>(refreshTokenThunk());
-        const newToken = store.getState().auth.token;
 
-        if (newToken) {
-          originalRequest.headers["Authorization"] = `Bearer ${newToken}`;
-          return axios(originalRequest);
+        try {
+          await store.dispatch<any>(refreshTokenThunk());
+          const newToken = store.getState().auth.token;
+
+          if (newToken) {
+            originalRequest.headers["Authorization"] = `Bearer ${newToken}`;
+            return axios(originalRequest);
+          }
+        } catch (err) {
+          store.dispatch(logout());
         }
-      }
 
-      return Promise.reject(err);
+        return Promise.reject(err);
+      }
     }
   );
 };
