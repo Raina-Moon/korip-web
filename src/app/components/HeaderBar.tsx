b8a9eccd70b56f1dd18f237ca513e95b2233df56
Raina Moon <mds6425@gmail.com>
2025-05-29 03:36:38 +0900

    ♻️ refactor: update HeaderBar component to improve language selection and user profile display

--- src/app/components/HeaderBar.tsx
+++ src/app/components/HeaderBar.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
+import { useAppSelector } from "../lib/store/hooks";
 
 const HeaderBar = () => {
-  const [select, setSelect] = useState("");
+  const [select, setSelect] = useState("en");
+  const [isHover, setIsHover] = useState(false);
+
   const router = useRouter();
+  
+  const user = useAppSelector((state) => state.auth.user);
 
   const languages = [
     { name: "English", code: "en" },
@@ -23,28 +28,42 @@ const HeaderBar = () => {
         />
       </div>
       <div className="flex items-center gap-4 pr-3">
-        <button value={select}>Language</button>
-        <button onClick={() => router.push("/login")}>
+        <select value={select} onChange={(e) => setSelect(e.target.value)}
+          className="border border-primary-800 rounded-sm px-2 py-1">
+          {languages.map((lang) => (
+            <option key={lang.code} value={lang.code}>
+              {lang.name}
+            </option>
+          ))}
+        </select>
+
+        <button onClick={() => router.push(user ? "/profile" : "/login")}
+          onMouseEnter={() => setIsHover(true)}
+          onMouseLeave={() => setIsHover(false)}
+          className="relative">
           <i className="bi bi-person-circle text-primary-800 text-3xl"></i>
+
+          {isHover && (
+            <div className="absolute right-0 mt-2 w-56 bg-white border border-gray-300 shadow-xl rounded p-3 z-50">
+              {!user ? (
+                <div>
+                  <p>Login to Continue</p>
+                  <button
+                  onClick={() => router.push("/login")}
+                  className="bg-primary-700 text-white rounded-md px-2 py-1 hover:bg-primary-500">Login</button>
+                </div>
+              ) : (
+                <div>
+                  <p>{user.nickname}</p>
+                  <p>{user.email}</p>
+                
+                </div>
+              )}
+            </div>
+          )}
         </button>
       </div>
 
-      {select && (
-        <div>
-          <select
-            value={select}
-            onChange={(e) => {
-              setSelect(e.target.value);
-            }}
-          >
-            {languages.map((lang) => (
-              <option key={lang.code} value={lang.code}>
-                {lang.name}
-              </option>
-            ))}
-          </select>
-        </div>
-      )}
     </div>
   );
 };
