1dd79909814e93bf082ff53e0d3317de56f5f403
Raina Moon <mds6425@gmail.com>
2025-05-28 18:33:39 +0900

    ♻️refactor : refactor refreshTokenThunk for improved error handling and code clarity

--- src/app/lib/auth/refrestTokenThunk.ts
+++ src/app/lib/auth/refrestTokenThunk.ts
@@ -1,17 +1,26 @@
-import axios from "axios"
+import axios from "axios";
 import { logout, setCredential } from "./authSlice";
 
 export const refreshTokenThunk = () => async (dispatch: any) => {
-    try {
-        const res = await axios.post("/v1/auth/refresh",{},{
-            withCredentials: true,
-        })
-        const newToken = res.data.accessToken;
-        localStorage.setItem("token", newToken);
+  try {
+    const res = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/v1/auth/refresh`,
+      {},
+      {
+        withCredentials: true,
+      }
+    );
+    const newToken = res.data.accessToken;
+    localStorage.setItem("token", newToken);
 
-        const user = JSON.parse(localStorage.getItem("user") || "null");
-        dispatch(setCredential({token: newToken, user}));
-    } catch (err) {
-        dispatch(logout());
+    const userString = localStorage.getItem("user");
+    const user = userString ? JSON.parse(userString) : null;
+    if (user) {
+      dispatch(setCredential({ token: newToken, user }));
+    } else {
+      dispatch(logout());
     }
-}
+  } catch (err) {
+    dispatch(logout());
+  }
+};
