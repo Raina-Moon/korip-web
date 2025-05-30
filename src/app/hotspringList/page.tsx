efb898f259226577ed297d7c87cf4c5633587d86
Raina Moon <mds6425@gmail.com>
2025-05-31 02:24:33 +0900

    ♻️ refactor: remove hardcoded sidoCenters and update getHotspringBySidoQuery usage

--- src/app/hotspringList/page.tsx
+++ src/app/hotspringList/page.tsx
@@ -3,34 +3,31 @@
 import React, { useState } from "react";
 import { useGetHotspringBySidoQuery } from "../lib/hotspring/hotspringApi";
 
-const sidoCenters: Record<string, { lat: number; lng: number }> = {
-  서울특별시: { lng: 126.95966555678402, lat: 37.53298542326604 },
-};
 
 const HotspringListPage = () => {
   const [selectedSido, setSelectedSido] = useState("서울특별시");
-  const coordinates = sidoCenters[selectedSido];
-  const { data, error } = useGetHotspringBySidoQuery(coordinates);
+  const { data, error } = useGetHotspringBySidoQuery();
 
   console.log("HotspringListPage data:", data);
 
   return (
     <div>
-      <select
+      {/* <select
         value={selectedSido}
         onChange={(e) => setSelectedSido(e.target.value)}
       >
-        {Object.keys(sidoCenters).map((sido) => (
+        {Object.keys().map((sido) => (
           <option key={sido} value={sido}>
             {sido}
           </option>
         ))}
-      </select>
+      </select> */}
       <ul>
         {data?.map((spring, idx) => (
           <li key={idx}>
             {spring.properties.uname} ({spring.properties.sido_name}{" "}
             {spring.properties.sigg_name})
+            {spring.properties.remark}
           </li>
         ))}
       </ul>
