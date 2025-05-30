e061e6737f8ddb5697a54239cb1d4de3c70713d9
Raina Moon <mds6425@gmail.com>
2025-05-30 20:02:12 +0900

    ✨ feat: integrate hotspringApi into Redux store for state management

--- src/app/lib/store/store.ts
+++ src/app/lib/store/store.ts
@@ -2,15 +2,17 @@ import { configureStore } from "@reduxjs/toolkit";
 import { authApi } from "../auth/authApi";
 import authReducer from "../auth/authSlice";
 import resetPasswordReducer from "../reset-password/resetPasswordSlice";
+import { hotspringApi } from "../hotspring/hotspringApi";
 
 export const store = configureStore({
     reducer: {
         auth:authReducer,
         resetPassword: resetPasswordReducer,
         [authApi.reducerPath]: authApi.reducer,
+        [hotspringApi.reducerPath] : hotspringApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(authApi.middleware),
+        getDefaultMiddleware().concat(authApi.middleware, hotspringApi.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>;
