13ad9d54bd220aac7603c18e6714cb7ffe56cfaf
Raina Moon <mds6425@gmail.com>
2025-06-13 22:22:21 +0900

    ♻️ refactor: replace user fetching logic with fetchCurrentUser for improved authentication handling

--- src/components/AuthLoader.tsx
+++ src/components/AuthLoader.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { setUserOnly } from "@/lib/auth/authSlice";
+import { fetchCurrentUser } from "@/lib/auth/authThunk";
 import axios from "axios";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
@@ -9,18 +9,7 @@ export default function AuthLoader() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/v1/auth/me`, {
-          withCredentials: true,
-        });
-        dispatch(setUserOnly(res.data.user));
-      } catch (error) {
-        console.error("Failed to fetch user:", error);
-        dispatch(setUserOnly(null));
-      }
-    };
-    fetchUser();
+    fetchCurrentUser()
   }, [dispatch]);
 
   return null;
