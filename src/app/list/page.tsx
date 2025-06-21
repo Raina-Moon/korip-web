c9cef8355ae5e70c1abe4ca8f64a87677fdc8ed2
Raina Moon <mds6425@gmail.com>
2025-06-21 17:41:59 +0900

    ✨ feat: rename Page component to ListPage and update region default value to "전체"

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,31 +1,54 @@
 "use client";
 
+import { useGetAvailableLodgeQuery } from "@/lib/hotspring/hotspringApi";
 import { useSearchParams } from "next/navigation";
 import React, { useState } from "react";
 
-const Page = () => {
-  const [searchList, setSearchList] = useState([]);
-
+const ListPage = () => {
   const searchParams = useSearchParams();
 
-  const region = searchParams.get("region") || "All";
+  const region = searchParams.get("region") || "전체";
   const checkin = searchParams.get("checkIn") || "Not specified";
   const checkout = searchParams.get("checkOut") || "Not specified";
   const room = searchParams.get("room") || "1";
   const adult = searchParams.get("adult") || "1";
   const children = searchParams.get("children") || "0";
 
+  const {
+    data: lodges,
+    isLoading,
+    isError,
+  } = useGetAvailableLodgeQuery({
+    region,
+    checkin,
+    checkout,
+    adult,
+    children,
+    room,
+  });
+
   return (
-    <div>
-      <p>{searchList.length}</p>
-      <p>Region: {region}</p>
-      <p>Check-in : {checkin}</p>
-      <p>Check-out : {checkout}</p>
-      <p>Room: {room}</p>
-      <p>Adult: {adult}</p>
-      <p>Children: {children}</p>
+    <div className="p-6">
+      <h1 className="text-xl font-semibold text-primary-900">검색 결과</h1>
+      {lodges?.length === 0 ? (
+        <p className="text-lg text-gray-600">검색 결과가 없습니다.</p>
+      ) : (
+        lodges?.map((lodge:any) => (
+          <div key={lodge.id} className="border p-4 mb-4 rounded-lg">
+            <h2 className="text-lg font-bold text-primary-900">{lodge.name}</h2>
+            <p className="text-gray-700">지역: {lodge.region}</p>
+            {lodge.roomTypes?.map((room:any) => (
+              <div key={room.id} className="mt-2 border p-2 bg-gray-50">
+                <p className="font-medium">{room.name}</p>
+                <p className="text-gray-700">성인 수: {lodge.adult}</p>
+                <p className="text-gray-700">어린이 수: {lodge.children}</p>
+              </div>
+            ))}
+          </div>
+        ))
+      )}
     </div>
   );
 };
 
-export default Page;
+export default ListPage;
