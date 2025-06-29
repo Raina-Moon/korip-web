bc6c1b18733bfad56c262ec438b4631a7f9d71bd
Raina Moon <mds6425@gmail.com>
2025-06-29 17:24:56 +0900

    ✨ add: implement reviews display with loading and error handling in ReviewsPage

--- src/app/profile/reviews/page.tsx
+++ src/app/profile/reviews/page.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useGetReviewsByUserIdQuery } from "@/lib/review/reviewApi";
+import { useAppSelector } from "@/lib/store/hooks";
+import { Review } from "@/types/reivew";
+import React from "react";
 
 const ReviewsPage = () => {
+  const { data: reviews, isLoading, isError } = useGetReviewsByUserIdQuery();
+  const nickname = useAppSelector((state) => state.auth.user?.nickname);
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      <h1>{nickname}'s Reviews</h1>
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>Error loading reviews</p>}
+      {reviews && reviews.length === 0 && <p>No reviews found</p>}
+      {reviews && reviews.length > 0 && (
+        <ul>
+          {reviews.map((review: Review) => (
+            <li key={review.id}>
+              <p>{review.rating} / 5</p>
+              <p>{review.content}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
 export default ReviewsPage;
