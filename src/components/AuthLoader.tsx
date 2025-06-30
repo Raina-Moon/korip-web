acf0f976553cee9ad851eaf7def56d09bb3094d6
Raina Moon <mds6425@gmail.com>
2025-06-30 17:40:48 +0900

    ♻️ refactor: streamline AuthLoader to improve access token management and user session handling

--- src/components/AuthLoader.tsx
+++ src/components/AuthLoader.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { setAccessToken } from "@/lib/auth/authSlice";
-import { fetchCurrentUser } from "@/lib/auth/authThunk";
+import { logout, setAccessToken } from "@/lib/auth/authSlice";
+import { fetchCurrentUser, tryRefreshSession } from "@/lib/auth/authThunk";
 import { useAppDispatch } from "@/lib/store/hooks";
 import { useEffect } from "react";
 
@@ -9,11 +9,25 @@ export default function AuthLoader() {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    const storedToken = localStorage.getItem("accessToken");
-    if (storedToken) {
-      dispatch(setAccessToken(storedToken));
-      dispatch(fetchCurrentUser());
-    }
+    const initializeAuth = async () => {
+      const storedToken = localStorage.getItem("accessToken");
+
+      if(storedToken) {
+        dispatch(setAccessToken(storedToken));
+        try {
+          await dispatch(fetchCurrentUser()).unwrap();
+        } catch (error) {
+          try {
+            const newToken = await dispatch(tryRefreshSession()).unwrap();
+            dispatch(setAccessToken(newToken));
+            await dispatch(fetchCurrentUser()).unwrap();
+          } catch (refreshErr) {
+            dispatch(logout());
+          }
+        }
+      }
+    };
+    initializeAuth();
   }, [dispatch]);
 
   return null;
