6653b5ec37bd82bd4c54dec097de875186a43578
Raina Moon <mds6425@gmail.com>
2025-07-12 16:51:55 +0900

    ✨ feat: add TicketReview interface to define ticket review properties

---
+++ src/types/ticketReview.ts
@@ -0,0 +1,12 @@
+export interface TicketReview {
+  id: number;
+  ticketTypeId: number;
+  userId: number;
+  rating: number;
+  comment?: string;
+  createdAt: string;
+  isHidden: boolean;
+  user: {
+    nickname: string;
+  };
+}
