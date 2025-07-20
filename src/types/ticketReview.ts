64fb93e476eb02a835d3f1e72185cdd15813fd82
Raina Moon <mds6425@gmail.com>
2025-07-20 23:25:14 +0900

    🐛 fix: add missing ticketReservationId field to TicketReview interface

--- src/types/ticketReview.ts
+++ src/types/ticketReview.ts
@@ -3,6 +3,7 @@ import { TicketReservation } from "./ticketReservation";
 export interface TicketReview {
   id: number;
   ticketTypeId: number;
+  ticketReservationId: number;
   userId: number;
   rating: number;
   comment?: string;
