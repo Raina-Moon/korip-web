5f72911c418003a3d511fc4ab8a600955cf0a938
Raina Moon <mds6425@gmail.com>
2025-07-20 18:18:03 +0900

    💄 ui: adjust layout in ReviewCard for improved styling consistency

--- src/components/ui/ReviewCard.tsx
+++ src/components/ui/ReviewCard.tsx
@@ -74,7 +74,7 @@ const ReviewCard = ({
 
   return (
     <div className="border rounded-lg p-4 bg-white shadow hover:shadow-md transition">
-      <div className="flex justify-between items-center mb-2 relative">
+<div className="mb-2 relative">
         {isTicketReview(review) && review.reservation && (
           <div className="text-sm text-gray-500 mb-2">
             <span className="mr-2">
@@ -89,7 +89,7 @@ const ReviewCard = ({
           </div>
         )}
 
-        <div className="flex items-center">
+  <div className="flex items-center">
           <span className="text-md text-primary-800 font-medium mr-2">
             {review.user?.nickname}
           </span>
