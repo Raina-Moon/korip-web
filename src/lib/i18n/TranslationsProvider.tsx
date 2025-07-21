89c404a5143e4999553b67e41479a0b524898cce
Raina Moon <mds6425@gmail.com>
2025-07-22 03:23:29 +0900

    ✨ feat: implement TranslationsProvider with i18n initialization and loading state

--- src/lib/i18n/TranslationsProvider.tsx
+++ src/lib/i18n/TranslationsProvider.tsx
@@ -1,11 +1,25 @@
 "use client";
 
-import { ReactNode } from "react";
+import { I18nextProvider } from "react-i18next";
+import { useEffect, useState } from "react";
+import i18n from "./i18n";
 
-export default function TranslationsProvider({
-  children,
-}: {
-  children: ReactNode;
-}) {
-  return <>{children}</>;
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function TranslationsProvider({ children }: Props) {
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    i18n.on("initialized", () => {
+      setReady(true);
+    });
+
+    if (i18n.isInitialized) setReady(true);
+  }, []);
+
+  if (!ready) return null;
+
+  return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>;
 }
