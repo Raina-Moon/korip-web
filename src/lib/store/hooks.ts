5eb3b9edbf40aaed02f5f9b6fb1155b19e6b4de6
Raina Moon <mds6425@gmail.com>
2025-07-22 16:48:22 +0900

    🔊 debug: add logging to useAppDispatch for better debugging

--- src/lib/store/hooks.ts
+++ src/lib/store/hooks.ts
@@ -1,5 +1,8 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "./store";
 
-export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppDispatch = () => {
+  console.log("🧨 useAppDispatch called");
+  return useDispatch<AppDispatch>();
+};
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
