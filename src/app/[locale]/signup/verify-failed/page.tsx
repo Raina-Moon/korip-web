c5e19fdf69b505155b1656247aa20e27509ba7f8
Raina Moon <mds6425@gmail.com>
2025-07-23 14:26:22 +0900

    ✨ feat: add English and Korean localization support for email verification failure page

--- src/app/[locale]/signup/verify-failed/page.tsx
+++ src/app/[locale]/signup/verify-failed/page.tsx
@@ -1,24 +1,24 @@
 "use client";
 
+import { useLocale } from "@/utils/useLocale";
 import { useRouter } from "next/navigation";
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 const VerifFailedPage = () => {
-    const router = useRouter();
+  const { t } = useTranslation("verif-fail");
+  const router = useRouter();
+  const locale = useLocale();
   return (
     <div>
-      <h1 className="text-3xl font-semibold text-primary-800">
-        Failed Email Verification
-      </h1>
-      <p className="text-primary-800 mt-4">
-        It seems that your email verification has failed. Please try signing up
-        again or contact support if you need assistance.
-      </p>
+      <h1 className="text-3xl font-semibold text-primary-800">{t("title")}</h1>
+
+      <p className="text-primary-800 mt-4">{t("description")}</p>
       <button
-        onClick={() => router.push("/signup/email")}
+        onClick={() => router.push(`/${locale}/signup/email`)}
         className="bg-primary-700 text-white px-4 py-2 rounded-md mt-4 hover:bg-primary-500"
       >
-        Go Back to Email Verification
+        {t("button")}
       </button>
     </div>
   );
