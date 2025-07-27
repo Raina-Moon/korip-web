073e704ddb8780f939d53de01bcd1d83ceb67946
Raina Moon <mds6425@gmail.com>
2025-07-27 15:32:57 +0900

    ✨ feat: integrate react-hot-toast for improved notification display

--- src/components/ClientApp.tsx
+++ src/components/ClientApp.tsx
@@ -8,15 +8,12 @@ import AuthLoader from "@/components/AuthLoader";
 import HeaderWrapper from "@/components/HeaderWrapper";
 import GlobalLoadingOverlay from "@/components/GlobalLoadingOverlay";
 import NavigationEvents from "@/lib/providers/NavigationEvents";
-
-export default function ClientApp({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import { Toaster } from "react-hot-toast";
+export default function ClientApp({ children }: { children: React.ReactNode }) {
   return (
     <Providers>
       <TranslationsProvider>
+        <Toaster position="top-center"/>
         <AuthLoader />
         <HeaderWrapper />
         <GlobalLoadingOverlay />
