53467cd7114afe67341a8ecd8b090b410f94669c
Raina Moon <mds6425@gmail.com>
2025-07-28 17:29:56 +0900

    ♻️ refactor: enhance type safety by updating ticketApi endpoint definitions

--- src/lib/ticket/ticketApi.ts
+++ src/lib/ticket/ticketApi.ts
@@ -3,15 +3,6 @@ import { prepareAuthHeaders } from "@/utils/prepareAuthHeaders";
 import { Ticket } from "@/types/ticket";
 import { TicketReview } from "@/types/ticketReview";
 
-interface TicketSearchParams {
-  lodgeId?: number;
-  date?: string;
-  region?: string;
-  adults?: number;
-  children?: number;
-  sort?: string;
-}
-
 interface TicketReviewCreateResponse {
   message: string;
   review: TicketReview;
@@ -36,7 +27,7 @@ export const ticketApi = createApi({
   }),
   tagTypes: ["TicketReviews"],
   endpoints: (builder) => ({
-    getAvailableTicket: builder.query<Ticket[], TicketSearchParams>({
+    getAvailableTicket: builder.query<Ticket[], any>({
       query: (params) => {
         const query = new URLSearchParams(
           Object.entries(params || {})
