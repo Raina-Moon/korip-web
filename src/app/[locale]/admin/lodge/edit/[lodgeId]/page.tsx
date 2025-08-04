cd63f3b59763f5a526a3646b729de3f06e46ac60
Raina Moon <mds6425@gmail.com>
2025-08-04 16:08:11 +0900

    💄 ui: enhance loading state during lodge update process in LodgeEditPage

--- src/app/[locale]/admin/lodge/edit/[lodgeId]/page.tsx
+++ src/app/[locale]/admin/lodge/edit/[lodgeId]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-export const runtime = 'edge';
+export const runtime = "edge";
 
 import LodgeForm from "@/components/ui/LodgeForm";
 import {
@@ -11,10 +11,11 @@ import { useAppDispatch, useAppSelector } from "@/lib/store/hooks";
 import { useLocale } from "@/utils/useLocale";
 import { ArrowLeft } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
 const LodgeEditPage = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const { lodgeId } = useParams();
   const id = Number(lodgeId);
   const dispatch = useAppDispatch();
@@ -32,8 +33,10 @@ const LodgeEditPage = () => {
   }, [dispatch, id]);
 
   const handleUpdateLodge = async (data: UpdateLodgePayload) => {
+    setIsSubmitting(true);
     const updateData = await dispatch(updateLodge(data));
 
+    setIsSubmitting(false);
     if (updateLodge.fulfilled.match(updateData)) {
       toast.success("숙소가 성공적으로 수정되었습니다.");
       router.push(`/${locale}/admin/lodge/${id}`);
@@ -43,7 +46,15 @@ const LodgeEditPage = () => {
   };
 
   return (
-    <div className="flex flex-col">
+    <div className="relative flex flex-col min-h-screen">
+      {isSubmitting && (
+        <div className="absolute z-50 inset-0 bg-black bg-opacity-60 flex items-center justify-center pointer-events-none">
+          <div className="text-white text-3xl font-bold animate-pulse pointer-events-auto">
+            숙소 수정 중...
+          </div>
+        </div>
+      )}
+
       <div className="relative flex items-center justify-center mx-24 mt-10">
         <div
           className="absolute left-0 p-2 cursor-pointer"
