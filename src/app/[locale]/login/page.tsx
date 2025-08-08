f56a99c7e2e8bf042e1e43de96baa263adec307d
Raina Moon <mds6425@gmail.com>
2025-08-08 21:58:06 +0900

    🐛 fix: ensure proper handling of redirect paths after login

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -139,7 +139,17 @@ const LoginPage = () => {
           localStorage.removeItem("pendingReservation");
           router.push(`/${locale}/reservation?${query}`);
         } else {
-          router.push(`/${redirectPath}`);
+          const isLocalePrefixed = redirectPath.startsWith(`/${locale}`);
+
+          router.push(
+            isLocalePrefixed
+              ? redirectPath
+              : `/${locale}${
+                  redirectPath.startsWith("/")
+                    ? redirectPath
+                    : `/${redirectPath}`
+                }`
+          );
         }
         dispatch(setRedirectAfterLogin(null));
       } else {
